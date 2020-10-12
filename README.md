bd60632c822824207ed336b40c0f03fa0c8e4181
fsimovski <filip.simovski92@gmail.com>
2020-10-12 18:49:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # general-cypress
+
+Open orpject and run 'node_modules/.bin/cypress open' in git bash
---
+++ cypress/support/index.js
@@ -0,0 +1,42 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+before(() => {
+    cy.log('Before')
+    cy.viewport(1920,1080)
+})
+
+beforeEach(() => {
+    // root-level hook
+    // runs before every test
+    cy.log('Before Each')
+    cy.visit(Cypress.env('base_url'))
+})
+
+afterEach(() => {
+    // runs after each test in the block
+    cy.log('After Each')
+})
+
+after(() => {
+    // runs once after all tests in the block
+    cy.log('After')
+})
